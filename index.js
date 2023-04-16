22321fbcd0e244c89534cc4adcf69b5b68fdf3f7
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2023-04-17 03:23:51 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const http = require('http');
+
+function requestListener(request, response) {
+    response.setHeader('Content-Type', 'text/html');
+    // response.setHeader('Content-Type', 'application/json');
+
+    if(request.method !== 'GET') {
+        response.statusCode = 405;
+        response.end('<h1>Method not allowed!</h1>');
+        // response.end(JSON.stringify({
+        //     status: 'fail',
+        //     message: 'method not allowed',
+        //     data: ,
+        // }))
+        return;
+    }
+
+    if(request.url === '/') {
+        response.statusCode = 200;
+        response.end('<h1>Halaman utama</h1>');
+        return;
+    }
+
+    if(request.url === '/article') {
+        response.statusCode = 200;
+        response.end('<h1>Halaman artikel</h1>');
+        return;
+    }
+
+    // response.setHeader('Content-Type', 'text/html');
+    response.end('<h1>404! alamat tidak ditemukan</h1>');
+}
+
+// membuat sebuah HTTP server
+const server = http.createServer(requestListener);
+
+// menjalankan server
+const port = 9000;
+server.listen(port, () => {
+    console.log(`Server berjalan pada http://localhost:${port}`);
+});
